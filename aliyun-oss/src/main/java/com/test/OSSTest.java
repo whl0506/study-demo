8fa660cf6c8d8ce682482b4b00516638380d1599
whl <whl0506@163.com>
2020-11-29 16:53:27 +0800

    Aliyun OSS demo演示创建bucket,修改bucket权限

---
+++ aliyun-oss/src/main/java/com/test/OSSTest.java
@@ -0,0 +1,53 @@
+package com.test;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.CannedAccessControlList;
+import org.junit.Test;
+
+/**
+ * 各种java SDK可参照阿里云官网OSS对象存储
+ * https://www.aliyun.com/product/oss
+ * 对象存储 OSS > SDK 示例 > Java SDK
+ */
+public class OSSTest {
+
+    // Endpoint以杭州为例，其它Region请按实际情况填写。
+    String endpoint = "oss-cn-shanghai.aliyuncs.com";
+    // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+    String accessKeyId = "LTAI4FeTQwLmQ11qxm4yzgXb";
+    String accessKeySecret = "whQjTWcK1GOZwPqPdmJ4VNM4XULqpy";
+    String bucketName = "oss-file-test";
+
+    @Test
+    public void testCreateBucket() {
+
+        // 创建OSSClient实例。
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+        // 创建存储空间。
+        if (!ossClient.doesBucketExist(bucketName)) {
+            ossClient.createBucket(bucketName);
+        }
+
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+    @Test
+    public void testModifyBucketAcl() {
+
+        // 创建OSSClient实例。
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+        // 创建存储空间。
+        if (ossClient.doesBucketExist(bucketName)) {
+            ossClient.setBucketAcl(bucketName,CannedAccessControlList.PublicRead);
+        }
+
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+}
